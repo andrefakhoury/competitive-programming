342ebbee22084af4f1a3c3a0f345a8100c024cdb
andrefakhoury <andreluis1704@gmail.com>
2020-12-26 15:24:21 -0300

    update

---
+++ cmake-build-debug/CMakeFiles/ATask.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../solves/tasks/ATask/ATask"
+  "../solves/tasks/ATask/ATask.pdb"
+  "CMakeFiles/ATask.dir/solves/tasks/ATask/ATask.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ATask.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/competitive_programming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/competitive_programming.dir/main.cpp.o"
+  "competitive_programming"
+  "competitive_programming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/competitive_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
