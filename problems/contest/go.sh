d6dd0622f5a741b5a4a969fd195f3cb899e4f628
andrefakhoury <andreluis1704@gmail.com>
2021-02-17 00:53:21 -0300

    upd prob

--- problems/contest/go.sh
+++
@@ -1,8 +0,0 @@
-if [ $# -eq 0 ]; then
-	echo "Invalid folder";
-else
-	mkdir $1 2> /dev/null
-	rm ./$1/*
-	cp /home/fakhoury/Documents/competitive-programming/lib/misc/environment/bash.sh ./$1/test.sh
-	cp /home/fakhoury/Documents/competitive-programming/lib/misc/environment/clion_template.cpp ./$1/sol.cpp
-fi
