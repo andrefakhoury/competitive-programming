c696a063a4fb487503bec8fdae0c7e57b6b6f282
andrefakhoury <andreluis1704@gmail.com>
2021-04-06 12:34:22 -0300

    Add more lib codes

---
+++ lib/DataStructures/BIT2D.hpp
@@ -0,0 +1,37 @@
+template<typename T>
+struct Bit2D {
+	int n, m;
+	vector<vector<T>> b;
+
+	Bit2D(int n_, int m_) :
+			n(n_+2), m(m_+2), b(n, vector<T>(m))
+	{}
+
+	// adds delta to position (x, y)
+	auto add(int x, int y, T delta) -> void {
+		x++, y++;
+		for (int i = x; i < n; i += i&-i) {
+			for (int j = y; j < n; j += j&-j) {
+				b[i][j] += delta;
+			}
+		}
+	}
+
+	// returns sum of rectangle [0, x] x [0, y]
+	auto get(int x, int y) -> T {
+		x++, y++;
+		T ans = 0;
+		for (int i = x; i; i -= i&-i) {
+			for (int j = y; j; j -= j&-j) {
+				ans += b[i][j];
+			}
+		}
+		return ans;
+	}
+
+	// returns sum of rectagle defined by [x1, x2] x [y1, y2]
+	// generalizable to N-dimensional BIT using inclusion-exclusion principle
+	auto get_rectangle(int x1, int x2, int y1, int y2) -> T {
+		return get(x2, y2) - get(x1-1, y2) - get(x2, y1-1) + get(x1-1, y1-1);
+	}
+};
---
+++ lib/DataStructures/MinQueue.hpp
@@ -0,0 +1,33 @@
+template<typename T, typename Cmp = less<T>>
+struct MinQueue {
+	constexpr static auto cmp = Cmp{};
+
+	deque<pair<T, int>> q;
+	int l = 0, r = 0;
+
+	auto push(T const& val) -> void {
+		while (!q.empty() and !cmp(q.back().first,val)) {
+			q.pop_back();
+		}
+		q.emplace_back(val, r++);
+	}
+
+	auto pop() -> void {
+		if (!q.empty() and q.front().second == l) {
+			q.pop_front();
+		}
+		l++;
+	}
+
+	auto get() -> T {
+		return q.front().first;
+	}
+
+	auto empty() -> bool {
+		return q.empty();
+	}
+
+	auto size() -> int {
+		return r-l;
+	}
+};
