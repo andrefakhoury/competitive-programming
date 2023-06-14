2038832d990d03c99b6c3b4b21ae0b4fe4e4247c
Andre Fakhoury <andreluis1704@gmail.com>
2023-06-14 01:00:02 -0300

    update wavelet

--- lib/DataStructures/WaveletTree.hpp
+++ lib/DataStructures/WaveletTree.hpp
@@ -1,14 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 /** Wavelet Tree data structure. Input array is 0 based */
 struct waveletTree {
 	int lo, hi, mi; // minimum, maximum and (lo+hi)/2 element on array
 	waveletTree *L, *R; // children
 	vector<int> mp; // map how many elements went left
 
-#define ML(i) (mp[i]) /* original mapLeft */
-#define MR(i) (i - mp[i] - 1) /* original mapRight */
+#define mapLeft(i) (mp[i]) /* original mapLeft */
+#define mapRight(i) (i - mapLeft(i)) /* original mapRight */
 
-	/** *beg points to the first element, *end points to after the last element (just like stl default functions)
-	  * Array values are between range [lo..hi] */
+	/** *beg points to the first element, *end points to after the last element (just like stl default functions).
+	 * Elements are in range [lo..hi], inclusive */
 	waveletTree(int *beg, int *end, int lo, int hi) { // O(nlogA)
 		L = R = NULL;
 		this->lo = lo;
@@ -30,58 +33,38 @@ struct waveletTree {
 		}
 	}
 
-	/** Frequency of k on range [1..i] */
-	int rank(int k, int i) {
-		if (lo == hi) return k == lo ? i : 0; // leaf node - just one value available
-		else { // keep searching
-			if (k <= mi) return L->rank(k, ML(i));
-			else return R->rank(k, MR(i));
-		}
-	}
-
-	/** Frequency of k in range [i..j] */
-	int rank(int k, int i, int j) {
-		return rank(k, j) - rank(k, i - 1); // similar to prefix sum
-	}
-
 	/** K-th smallest element on range[l..r] */
 	int kthSmallest(int k, int l, int r) {
 		if (l > r) return -1; // out of bounds
 		if (lo == hi) return lo; // leaf node
 
-		int inLeft = ML(r) - ML(l-1);
-		if (k <= inLeft) return L->kthSmallest(k, ML(l-1)+1, ML(r));
-		else return R->kthSmallest(k-inLeft, MR(l-1)+1, MR(r));
+		int inLeft = mapLeft(r) - mapLeft(l-1);
+		if (k <= inLeft) return L->kthSmallest(k, mapLeft(l-1)+1, mapLeft(r));
+		else return R->kthSmallest(k-inLeft, mapRight(l-1)+1, mapRight(r));
 	}
 
-	/** Qtt of elements between [x..y] in array[l..r] */
+	/** Frequency of elements between [x..y] in array[l..r] */
 	int rangeCount(int x, int y, int l, int r) {
 		if (l > r || lo > y || hi < x) return 0; // out of bounds
 		if (lo >= x && hi <= y) return r - l + 1; // total fit
-		return L->rangeCount(x, mi, ML(l-1)+1, ML(r)) +  // same idea of segtree
-			   R->rangeCount(mi+1, y, MR(l-1)+1, MR(r));
-	}
 
-	/** Find minimum X such that freq[x] on interval l..r is strictly greater thank k */
-	int findFreqGreater(int k, int l, int r) {
-		if (r - l + 1 <= k) return -1;
-		if (lo == hi) return lo;
-		int cur = L->findFreqGreater(k, ML(l-1)+1, ML(r));
-		if (cur != -1) return cur;
-		return R->findFreqGreater(k, MR(l-1)+1, MR(r));
+		if (mi >= y) return L->rangeCount(x, y, mapLeft(l-1)+1, mapLeft(r));
+		else if (mi <= x) return R->rangeCount(x, y, mapRight(l-1)+1, mapRight(r));
+		else return L->rangeCount(x, mi, mapLeft(l-1)+1, mapLeft(r)) +
+			   R->rangeCount(mi+1, y, mapRight(l-1)+1, mapRight(r));
 	}
 
 	/** Swap elements a[i] and a[i+1] */
 	void swapContiguous(int i) {
 		if (lo == hi) return; // leaf node, no need to swap
 
-		bool iLeft = ML(i) == ML(i-1) + 1; // if a[i] <= mi
-		bool i1Left = (ML(i+1)) == (ML(i) + 1); // if a[i+1] <= mi
+		bool iLeft = mapLeft(i) == mapLeft(i-1) + 1; // if a[i] <= mi
+		bool i1Left = (mapLeft(i+1)) == (mapLeft(i) + 1); // if a[i+1] <= mi
 
-		if (iLeft && !i1Left) ML(i)--;
-		else if (iLeft && i1Left) L->swapContiguous(ML(i));
-		else if (!iLeft && i1Left) ML(i)++;
-		else R->swapContiguous(MR(i));
+		if (iLeft && !i1Left) mapLeft(i)--;
+		else if (!iLeft && i1Left) mapLeft(i)++;
+		else if (iLeft && i1Left) L->swapContiguous(mapLeft(i));
+		else R->swapContiguous(mapRight(i));
 	}
 
 	/** Push element k to end of array */
@@ -106,4 +89,38 @@ struct waveletTree {
 		if (L) delete L;
 		if (R) delete R;
 	}
+
+#undef mapLeft
+#undef mapRight
 };
+
+const int MAXN = 1e5 + 5;
+int a[MAXN];
+
+// https://www.spoj.com/problems/ILKQUERY/
+int main() {
+	int n, q; scanf("%d%d", &n, &q);
+
+	vector<int> all;
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", a + i);
+		all.push_back(a[i]);
+	}
+	sort(all.begin(), all.end());
+	all.erase(unique(all.begin(), all.end()), all.end());
+	for (int i = 1; i <= n; i++)
+		a[i] = lower_bound(all.begin(), all.end(), a[i]) - all.begin();
+
+	int N = all.size();
+	vector<vector<int>> pos(N);
+	for (int i = 1; i <= n; i++) pos[a[i]].push_back(i - 1);
+
+	waveletTree T(a+1, a+n+1, 0, all.size());
+	while(q--) {
+		int k, i, l; scanf("%d%d%d", &k, &i, &l);
+		int ans = T.kthSmallest(k, 1, i + 1);
+		printf("%d\n", l <= (int) pos[ans].size() ? pos[ans][l - 1] : -1);
+	}
+
+	return 0;
+}
